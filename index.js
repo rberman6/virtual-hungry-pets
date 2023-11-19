f5d10fc1bfd3d3b9c203542f2bf4f02d12d29f5b
rberman6 <rittuberman@gmail.com>
2023-11-19 10:13:04 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,543 @@
+// ARRAY OF 50 PET OBJECTS;
+const pets = [
+  {
+    name: "Whiskers",
+    id: 1,
+    emoji: "🐱",
+    hunger: 0,
+    love: 100,
+    alive: true,
+    epitaph: "Forever chasing mice in kitty paradise.",
+  },
+  {
+    name: "Sunny",
+    id: 2,
+    emoji: "🦧",
+    hunger: 0,
+    love: 100,
+    alive: true,
+    epitaph: "Brought warmth to our hearts.",
+  },
+  {
+    name: "Bubbles",
+    id: 3,
+    emoji: "🦮",
+    hunger: 0,
+    love: 100,
+    alive: true,
+    epitaph: "Popped into our lives and left bubbles of joy.",
+  },
+  {
+    name: "Cocoa",
+    id: 4,
+    emoji: "🐕‍🦺",
+    hunger: 0,
+    love: 100,
+    alive: true,
+    epitaph: "Sweet as chocolate, forever in our hearts.",
+  },
+  {
+    name: "Rocket",
+    id: 5,
+    emoji: "🦌",
+    hunger: 0,
+    love: 100,
+    alive: true,
+    epitaph: "Zoomed through life with boundless energy.",
+  },
+  {
+    name: "Luna",
+    id: 6,
+    emoji: "🐮",
+    hunger: 0,
+    love: 100,
+    alive: true,
+    epitaph: "Lit up our nights and left a trail of stardust.",
+  },
+  {
+    name: "Olive",
+    id: 7,
+    emoji: "🐇",
+    hunger: 0,
+    love: 100,
+    alive: true,
+    epitaph: "A little olive, a lot of love.",
+  },
+  {
+    name: "Thunder",
+    id: 8,
+    emoji: "🐕",
+    hunger: 0,
+    love: 100,
+    alive: true,
+    epitaph: "Thunderous paws, gentle heart.",
+  },
+  {
+    name: "Mochi",
+    id: 8,
+    emoji: "🐰",
+    hunger: 0,
+    love: 100,
+    alive: true,
+    epitaph: "Soft as mochi, sweet as memories.",
+  },
+  {
+    name: "Jazz",
+    id: 10,
+    emoji: "🐒",
+    hunger: 0,
+    love: 100,
+    alive: true,
+    epitaph: "Played the melody of love in our hearts.",
+  },
+  {
+    name: "Pebbles",
+    id: 11,
+    emoji: "🐶",
+    hunger: 0,
+    love: 100,
+    alive: true,
+    epitaph: "A rock in our lives, forever missed.",
+  },
+  {
+    name: "Echo",
+    id: 12,
+    emoji: "🐥",
+    hunger: 0,
+    love: 100,
+    alive: true,
+    epitaph: "Whispering memories of joy.",
+  },
+  {
+    name: "Maple",
+    id: 13,
+    emoji: "🦩",
+    hunger: 0,
+    love: 100,
+    alive: true,
+    epitaph: "Fell from the tree of our lives, leaving golden memories.",
+  },
+  {
+    name: "Noodle",
+    id: 14,
+    emoji: "🦎",
+    hunger: 0,
+    love: 100,
+    alive: true,
+    epitaph: "Slithered into our hearts, forever coiled in love.",
+  },
+  {
+    name: "Gizmo",
+    id: 15,
+    emoji: "🦉",
+    hunger: 0,
+    love: 100,
+    alive: true,
+    epitaph: "Pressed the buttons of happiness in our lives.",
+  },
+  {
+    name: "Peach",
+    id: 16,
+    emoji: "🐅",
+    hunger: 0,
+    love: 100,
+    alive: true,
+    epitaph: "Juicy memories, ripe with love.",
+  },
+  {
+    name: "Zephyr",
+    id: 17,
+    emoji: "🐩",
+    hunger: 0,
+    love: 100,
+    alive: true,
+    epitaph: "A gentle breeze that swept away into eternity.",
+  },
+  {
+    name: "Pixel",
+    id: 18,
+    emoji: "🦄",
+    hunger: 0,
+    love: 100,
+    alive: true,
+    epitaph: "A pixel in our heartscape, forever bright.",
+  },
+  {
+    name: "Truffle",
+    id: 19,
+    emoji: "🐹",
+    hunger: 0,
+    love: 100,
+    alive: true,
+    epitaph: "Dark and sweet, like the memories you left behind.",
+  },
+  {
+    name: "Phoenix",
+    id: 20,
+    emoji: "🐙",
+    hunger: 0,
+    love: 100,
+    alive: true,
+    epitaph: "Rose from the ashes of joy, a fiery companion.",
+  },
+  {
+    name: "Nimbus",
+    id: 21,
+    emoji: "🐳",
+    hunger: 0,
+    love: 100,
+    alive: true,
+    epitaph: "Floated into the clouds, leaving a sky full of memories.",
+  },
+  {
+    name: "Poppy",
+    id: 22,
+    emoji: "🐡",
+    hunger: 0,
+    love: 100,
+    alive: true,
+    epitaph: "Blossomed in our lives, forever in bloom.",
+  },
+  {
+    name: "Jigsaw",
+    id: 23,
+    emoji: "🐢",
+    hunger: 0,
+    love: 100,
+    alive: true,
+    epitaph: "A piece of our hearts, forever missing.",
+  },
+  {
+    name: "Zen",
+    id: 24,
+    emoji: "🦆",
+    hunger: 0,
+    love: 100,
+    alive: true,
+    epitaph: "Found peace in the serenity of our love.",
+  },
+  {
+    name: "Sable",
+    id: 25,
+    emoji: "🐨",
+    hunger: 0,
+    love: 100,
+    alive: true,
+    epitaph: "Dug deep into our hearts, leaving paw prints of love.",
+  },
+  {
+    name: "Cosmo",
+    id: 26,
+    emoji: "🦊",
+    hunger: 0,
+    love: 100,
+    alive: true,
+    epitaph: "Rocketed into our world, now among the stars.",
+  },
+  {
+    name: "Cinnamon",
+    id: 27,
+    emoji: "🐈",
+    hunger: 0,
+    love: 100,
+    alive: true,
+    epitaph: "Sprinkled warmth and sweetness into our lives.",
+  },
+  {
+    name: "Neptune",
+    id: 28,
+    emoji: "🐈‍⬛",
+    hunger: 0,
+    love: 100,
+    alive: true,
+    epitaph: "Sailed away into the sea of memories.",
+  },
+  {
+    name: "Ziggy",
+    id: 29,
+    emoji: "🐕",
+    hunger: 0,
+    love: 100,
+    alive: true,
+    epitaph: "Painted our lives with vibrant colors of joy.",
+  },
+  {
+    name: "Mango",
+    id: 30,
+    emoji: "🦁",
+    hunger: 0,
+    love: 100,
+    alive: true,
+    epitaph: "Ripe with love, forever in our hearts.",
+  },
+  {
+    name: "Ripple",
+    id: 31,
+    emoji: "🦓",
+    hunger: 0,
+    love: 100,
+    alive: true,
+    epitaph: "Created waves of happiness that echo in our hearts.",
+  },
+  {
+    name: "Daisy",
+    id: 32,
+    emoji: "🐁",
+    hunger: 0,
+    love: 100,
+    alive: true,
+    epitaph: "In the garden of memories, forever in bloom.",
+  },
+  {
+    name: "Frodo",
+    id: 33,
+    emoji: "🫏",
+    hunger: 0,
+    love: 100,
+    alive: true,
+    epitaph: "Embarked on an epic journey, leaving paw prints in our hearts.",
+  },
+  {
+    name: "Twix",
+    id: 34,
+    emoji: "🐦",
+    hunger: 0,
+    love: 100,
+    alive: true,
+    epitaph: "A treat of joy in our lives, forever missed.",
+  },
+  {
+    name: "Galaxy",
+    id: 35,
+    emoji: "🐼",
+    hunger: 0,
+    love: 100,
+    alive: true,
+    epitaph: "Sparkled in our universe, now a constellation of memories.",
+  },
+  {
+    name: "Tinkerbell",
+    id: 36,
+    emoji: "🐧",
+    hunger: 0,
+    love: 100,
+    alive: true,
+    epitaph: "Spread magic and pixie dust, now flying among the stars.",
+  },
+  {
+    name: "Basil",
+    id: 37,
+    emoji: "🦚",
+    hunger: 0,
+    love: 100,
+    alive: true,
+    epitaph: "Seasoned our lives with love, forever fresh in our hearts.",
+  },
+  {
+    name: "Stormy",
+    id: 38,
+    emoji: "🐟",
+    hunger: 0,
+    love: 100,
+    alive: true,
+    epitaph: "Rained love upon us, now a gentle drizzle of memories.",
+  },
+  {
+    name: "Snickers",
+    id: 39,
+    emoji: "🐻‍❄️",
+    hunger: 0,
+    love: 100,
+    alive: true,
+    epitaph: "A sweet laugh in our lives, forever echoing.",
+  },
+  {
+    name: "Nova",
+    id: 40,
+    emoji: "🐬",
+    hunger: 0,
+    love: 100,
+    alive: true,
+    epitaph: "A bright star in our constellation of memories.",
+  },
+  {
+    name: "Harmony",
+    id: 41,
+    emoji: "🦜",
+    hunger: 0,
+    love: 100,
+    alive: true,
+    epitaph: "Played the symphony of joy in our hearts.",
+  },
+  {
+    name: "Bolt",
+    id: 42,
+    emoji: "🦫",
+    hunger: 0,
+    love: 100,
+    alive: true,
+    epitaph: "Struck our hearts with lightning love.",
+  },
+  {
+    name: "Waffles",
+    id: 43,
+    emoji: "🐘",
+    hunger: 0,
+    love: 100,
+    alive: true,
+    epitaph: "A breakfast of joy, forever on our hearts' menu.",
+  },
+  {
+    name: "Athena",
+    id: 44,
+    emoji: "🦢",
+    hunger: 0,
+    love: 100,
+    alive: true,
+    epitaph: "Wise as an owl, forever watching over us.",
+  },
+  {
+    name: "S'mores",
+    id: 45,
+    emoji: "🐿️",
+    hunger: 0,
+    love: 100,
+    alive: true,
+    epitaph: "Melted our hearts with sweetness, now a warm memory.",
+  },
+  {
+    name: "Coral",
+    id: 46,
+    emoji: "🐠",
+    hunger: 0,
+    love: 100,
+    alive: true,
+    epitaph: "Swam into our hearts, leaving a coral reef of memories.",
+  },
+  {
+    name: "Thunderbolt",
+    id: 47,
+    emoji: "🦃",
+    hunger: 0,
+    love: 100,
+    alive: true,
+    epitaph: "A lightning strike of love, forever electrifying.",
+  },
+  {
+    name: "Pumpkin",
+    id: 48,
+    emoji: "🦒",
+    hunger: 0,
+    love: 100,
+    alive: true,
+    epitaph: "Carved a place in our hearts, forever autumn.",
+  },
+  {
+    name: "Breeze",
+    id: 49,
+    emoji: "🐷",
+    hunger: 0,
+    love: 100,
+    alive: true,
+    epitaph: "A gentle breeze that rustled through our memories.",
+  },
+  {
+    name: "Panda",
+    id: 50,
+    emoji: "🐼",
+    hunger: 0,
+    love: 100,
+    alive: true,
+    epitaph: "A black and white bundle of joy, forever bamboo-zled us.",
+  },
+];
+
+// GENERATE RANDOM OBJECT
+let randomObj;
+function randomObject() {
+  let randomIndex = Math.floor(Math.random() * 51);
+  randomObj = pets[randomIndex];
+  console.log(randomObj);
+  return randomObj;
+}
+// randomObject();
+
+let endGame;
+
+// RENDER SAME PET OBJECT EVERY 30 SECS
+function render() {
+  randomObject();
+  const mainContainer = document.querySelector("#main-container");
+  // create container for each pet details
+  const petContainer = document.createElement("div");
+  petContainer.id = "pet-container";
+  petContainer.className = "alive";
+  mainContainer.appendChild(petContainer);
+  // create emoji
+  const emojiDiv = document.createElement("div");
+  emojiDiv.id = "emoji";
+  emojiDiv.className = "";
+  emojiDiv.textContent = `${randomObj.emoji}`;
+  petContainer.appendChild(emojiDiv);
+  // creating heading for pet name
+  const heading = document.createElement("h3");
+  heading.textContent = `${randomObj.name}`;
+  petContainer.appendChild(heading);
+  // create hunger label
+  const labelHunger = document.createElement("p");
+  labelHunger.textContent = "Hunger:";
+  petContainer.appendChild(labelHunger);
+  // create meter hunger bar
+  const hungerDiv = document.createElement("div");
+  hungerDiv.classList = "meter-bar";
+  petContainer.appendChild(hungerDiv);
+  const hungerMeter = document.createElement("div");
+  hungerMeter.className = "hungry";
+  hungerDiv.appendChild(hungerMeter);
+  // create love label
+  const labelLove = document.createElement("p");
+  labelLove.textContent = "Love:";
+  petContainer.appendChild(labelLove);
+  // create meter love bar
+  const loveDiv = document.createElement("div");
+  loveDiv.classList = "meter-bar";
+  petContainer.appendChild(loveDiv);
+  const loveMeter = document.createElement("div");
+  loveMeter.className = "love";
+  loveDiv.appendChild(loveMeter);
+  //  create feed me button
+  const button = document.createElement("button");
+  button.innerText = "Feed me 🍞";
+  petContainer.appendChild(button);
+  document.querySelector("button");
+
+  // function to run hunger and love meters
+  let hunger = 0;
+  let interval = setInterval(function () {
+    button.addEventListener("click", function () {
+      hunger = 0;
+    });
+    hunger = hunger + 1;
+    if (hunger === 100) {
+      clearInterval(interval);
+      // activate the psuedo element?
+    }
+    hungerMeter.style.width = `${hunger}px`;
+  }, 1000);
+  let love = 100;
+  setInterval(function () {
+    emojiDiv.addEventListener("click", function () {
+      love = 101;
+    });
+    love = love - 1;
+    if (love === 0) {
+      clearInterval(interval);
+      // replace old class with new class "died"
+    }
+    loveMeter.style.width = `${love}px`;
+  }, 1000);
+}
+
+render();
+
+// setInterval(render, 5000);
